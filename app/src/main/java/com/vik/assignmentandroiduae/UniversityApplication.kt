9b32f42b49800361e8164f105b9a8b117bffd323
viveksingh <470059@petco.com>
2024-08-08 18:21:21 +0530

    First commit

---
+++ app/src/main/java/com/vik/assignmentandroiduae/UniversityApplication.kt
@@ -0,0 +1,9 @@
+package com.vik.assignmentandroiduae
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class UniversityApplication : Application() {
+
+}
